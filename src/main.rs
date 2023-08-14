69589a5158d8c4af91dec5c91cc837b31b17c827
Bokayy X <blabsy8@gmail.com>
2023-08-14 22:15:11 +0200

    Simplest as can be

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,11 @@
 fn main() {
     let x: &str = "abc";
-    let y: &str = "bc";
-    let mut x_as_array: Vec<char> = Vec::new();
-    let mut y_as_array: Vec<char> = Vec::new();
-    let mut flag_true = false;
+    let y: &str = "xs";
 
-    for character in x.chars(){
-        x_as_array.push(character);
+    if x.ends_with(y){
+        dbg!("true");
     }
-    dbg!(x_as_array);
-
-    for character in y.chars(){
-        y_as_array.push(character);
+    else {
+        dbg!("false");
     }
-    dbg!(y_as_array);
-
-    for (index, character) in y.char_indices(){
-        if (&character == &x_as_array[index]){
-            flag_true = true;
-        }
-        else{
-        flag_true = false;}
-    }
-
-    dbg!(flag_true);
 }
